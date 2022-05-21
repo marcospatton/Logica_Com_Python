be98cd32e088102ff0dc0c57ddae489fe897146b
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-05-21 08:34:02 -0300

    Add files via upload

---
+++ aula2condicionais.py
@@ -0,0 +1,20 @@
+#Condicionais ainda
+    #Encontre o maior  entre 2 números
+
+#Algortmo
+#input primeiro_valor
+#input segundo_valor
+#if primeiro_valor > segundo_valor
+    #print o primeiro valor é maior
+#else
+    #print o segundo valor é maior
+
+
+primeiro_valor = input('Digite o 1º valor: ')
+segundo_valor = input('Digite o 2º valor: ')
+
+if int(primeiro_valor) > int (segundo_valor):
+    print('O primeiro valor e maior! ')
+else:
+    print('O segundo valor é maior !')
+
---
+++ aula3.py
@@ -0,0 +1,9 @@
+#Laços de Repetição + Listas
+
+for palavra in range(1,4):
+    print('Carregando')
+
+    for item in range(1,21):
+        print(item)
+    for item in range(1,20,2):
+        print(item)
